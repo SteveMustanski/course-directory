c5fbd81d789123b92ecb858df451c922664c65ca
Steve Mustanski <steve.mustanski@gmail.com>
2018-12-09 14:21:11 -0500

    added routes for home, about, teachers, and courses components.  minor readme updates too

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # course-directory
-A project for a fictional course directory using React
+A project for a fictional course directory using React with a focus on routing
+
+## Dependencies
+* react
+* react-dom
+* react-router-dom
+
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
+
+// Import app components
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
 
 const App = () => (
-  <div className="container">
+  <BrowserRouter>
+    <div className="container">
+    <Header />
+      {/* exact tells router to only render when the path matches exactly */}
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route exact path="/teachers" component={Teachers} />
+      <Route path="/courses" component={Courses} />
+    </div>
 
-  </div>
+  </BrowserRouter>
 );
 
 export default App;
