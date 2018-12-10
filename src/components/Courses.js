b4289865fb8db2095eb489d4b011bd0ff952b97d
Steve Mustanski <steve.mustanski@gmail.com>
2018-12-10 13:02:07 -0500

    added working navigation on courses component

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
+
+// Import app components
+import HTML from './courses/HTML';
+import CSS from './courses/CSS';
+import JavaScript from './courses/JavaScript';
 
 const Courses = () => (
   <div className="main-content courses">
@@ -12,6 +21,16 @@ const Courses = () => (
     </div>
     
     {/* Write routes here... */}
+
+      <BrowserRouter>
+    <div className="container">
+      {/* exact tells router to only render when the path matches exactly */}
+      <Route exact path="/courses/html" component={HTML} />
+      <Route exact path="/courses/css" component={CSS} />
+      <Route exact path="/courses/javascript" component={JavaScript} />
+    </div>
+
+  </BrowserRouter>
   </div>
 );
 
