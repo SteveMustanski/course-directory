9763016059efcec52f1fb9cb8619b9c028f56187
Steve Mustanski <steve.mustanski@gmail.com>
2018-12-10 13:22:08 -0500

    added route props to better handle routing

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -10,23 +10,23 @@ import HTML from './courses/HTML';
 import CSS from './courses/CSS';
 import JavaScript from './courses/JavaScript';
 
-const Courses = () => (
+const Courses = ({match}) => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><NavLink to='/courses/html'>HTML</NavLink></li>
-        <li><NavLink to='/courses/css'>CSS</NavLink></li>
-        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
+        <li><NavLink to={`${match.url}/html`}>HTML</NavLink></li>
+        <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
+        <li><NavLink to={`${match.url}/javascript`}>JavaScript</NavLink></li>
       </ul>
     </div>
 
     {/* nav for courses selecitons */}
-      <Route exact path="/courses" render={ () => <Redirect to='/courses/html'/>} />
+      <Route exact path={match.url} render={ () => <Redirect to={`${match.path}/html`}/>} />
       
-      <Route path="/courses/html" component={HTML} />
-      <Route path="/courses/css" component={CSS} />
-      <Route path="/courses/javascript" component={JavaScript} />
+      <Route path={`${match.path}/html`} component={HTML} />
+      <Route path={`${match.path}/css`} component={CSS} />
+      <Route path={`${match.path}/javascript`} component={JavaScript} />
     </div>
 
 
