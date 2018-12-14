7ff982b497e794910b9093d9cfea8cd3635ec13d
Steve Mustanski <steve.mustanski@gmail.com>
2018-12-14 12:13:41 -0500

    added switch component and added notfound component to routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
   BrowserRouter,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom';
 
 // Import app components
@@ -10,16 +11,22 @@ import Home from './Home';
 import About from './About';
 import Teachers from './Teachers';
 import Courses from './Courses';
+import NotFound from './NotFound';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
-    <Header />
-      {/* exact tells router to only render when the path matches exactly */}
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route exact path="/teachers" component={Teachers} />
-      <Route path="/courses" component={Courses} />
+      <Header />
+      {/* Switch will look for a path that matches
+          and fall through to the default route if it doesn't find a match */}
+      <Switch>
+        {/* exact tells router to only render when the path matches exactly */}
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route exact path="/teachers" component={Teachers} />
+        <Route path="/courses" component={Courses} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
 
   </BrowserRouter>
